64309bfb376ed0dabd02e72b6ad5e8392b389312
mactavishz <maczhanchao@yahoo.com>
2026-03-16 23:30:54 +0100

    Use util for tmp dir

--- pkg/manager/manager.go
+++ pkg/manager/manager.go
@@ -22,15 +22,8 @@ import (
 
 var (
 	// TmpDir can be overridden via TF_TMP_DIR environment variable
-	TmpDir = getEnvOrDefault("TF_TMP_DIR", "./tmp")
+	TmpDir = util.GetEnvOrDefault("TF_TMP_DIR", "/var/lib/tinyfaas/tmp")
 )
-
-func getEnvOrDefault(key, defaultValue string) string {
-	if value := os.Getenv(key); value != "" {
-		return value
-	}
-	return defaultValue
-}
 
 type ManagementService struct {
 	id               string
