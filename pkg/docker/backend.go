9f0b182ac728a78b61a9969541a84089e412959a
mactavishz <maczhanchao@yahoo.com>
2026-03-19 15:26:23 +0100

    Move docker backend logic to a separate file

---
+++ pkg/docker/backend.go
@@ -0,0 +1,285 @@
+package docker
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/OpenFogStack/tinyFaaS/pkg/manager"
+	"github.com/OpenFogStack/tinyFaaS/pkg/util"
+	"github.com/google/uuid"
+	"github.com/moby/go-archive"
+	"github.com/moby/go-archive/compression"
+	"github.com/moby/moby/client"
+	"github.com/moby/moby/client/pkg/jsonmessage"
+	"github.com/moby/term"
+	"go.uber.org/zap"
+)
+
+type DockerBackend struct {
+	client       *client.Client
+	tinyFaaSID   string
+	gatewayIP    string // host gateway IP for --add-host (where the Gateway runs)
+	publicDomain string // public domain for --add-host (e.g., tinyfaas.com)
+	gatewayPort  string // gateway port injected into containers as TINYFAAS_GATEWAY_URL
+	logger       *zap.Logger
+}
+
+func New(tinyFaaSID string, logger *zap.Logger) *DockerBackend {
+	// create docker client
+	client, err := client.New(client.FromEnv)
+	if err != nil {
+		logger.Fatal("error creating docker client", zap.Error(err))
+		return nil
+	}
+
+	db := &DockerBackend{
+		client:     client,
+		tinyFaaSID: tinyFaaSID,
+		logger:     logger,
+	}
+
+	// Get public domain from environment variable
+	db.publicDomain = os.Getenv("TINYFAAS_PUBLIC_DOMAIN")
+	if db.publicDomain == "" {
+		db.publicDomain = DEFAULT_PUBLIC_DOMAIN
+		logger.Info("TINYFAAS_PUBLIC_DOMAIN not set, using default", zap.String("publicDomain", db.publicDomain))
+	}
+
+	// Get gateway IP from environment variable
+	// This should be the host IP where the Gateway is running
+	db.gatewayIP = os.Getenv("TINYFAAS_GATEWAY_IP")
+	if db.gatewayIP == "" {
+		// Default to host.docker.internal for Docker Desktop
+		// On Linux, this might need to be set explicitly to the host IP
+		db.gatewayIP = "host-gateway"
+		logger.Info("TINYFAAS_GATEWAY_IP not set, using default", zap.String("gatewayIP", db.gatewayIP))
+	}
+
+	// Get gateway port from environment variable (same var used by the gateway service)
+	db.gatewayPort = os.Getenv("TF_GATEWAY_PORT")
+	if db.gatewayPort == "" {
+		db.gatewayPort = "80"
+		logger.Info("TF_GATEWAY_PORT not set, using default", zap.String("gatewayPort", db.gatewayPort))
+	}
+
+	// Note: Runtime base images must be pre-built using 'make build-runtime-images'
+	// or the build script. Manager expects images to already exist.
+	logger.Info("verifying runtime base images...")
+
+	// Verify all runtime base images exist
+	if err := db.verifyRuntimeImages(); err != nil {
+		logger.Fatal("runtime base images not found, please run 'make build-runtime-images' first", zap.Error(err))
+		return nil
+	}
+
+	logger.Info("all runtime base images verified")
+	return db
+}
+
+// verifyRuntimeImages checks that all required runtime base images exist
+func (db *DockerBackend) verifyRuntimeImages() error {
+	var missing []string
+	for _, runtime := range RUNTIMES {
+		baseImageTag := db.getRuntimeBaseImage(runtime)
+		_, err := db.client.ImageInspect(context.Background(), baseImageTag)
+		if err != nil {
+			missing = append(missing, baseImageTag)
+		} else {
+			db.logger.Info("found runtime base image", zap.String("image", baseImageTag))
+		}
+	}
+
+	if len(missing) > 0 {
+		return fmt.Errorf("missing runtime images: %v", missing)
+	}
+
+	return nil
+}
+
+func (db *DockerBackend) Stop() error {
+	return nil
+}
+
+// getRuntimeBaseImage returns the tag for a runtime's base image
+// Assumes the image was pre-built and exists in Docker
+func (db *DockerBackend) getRuntimeBaseImage(runtime string) string {
+	return fmt.Sprintf("tinyfaas-runtime-%s", runtime)
+}
+
+func (db *DockerBackend) Create(name string, env string, replicas int, filedir string, envs map[string]string, labels map[string]string, limits manager.ResourceLimits) (manager.Handler, error) {
+
+	// make a unique function name by appending uuid string to function name
+	ctx := context.Background()
+	uuid, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	dh := &dockerHandler{
+		name:        name,
+		env:         env,
+		client:      db.client,
+		replicas:    replicas,
+		nanoCPUs:    limits.NanoCPUs,
+		memoryBytes: limits.MemoryBytes,
+		containers:  make([]string, 0, replicas),
+		handlerIPs:  make([]string, 0, replicas),
+		isRunning:   false,
+		labels:      labels,
+		logger:      db.logger,
+	}
+
+	dh.uniqueName = name + "-" + uuid.String()
+	db.logger.Info("creating function", zap.String("name", dh.name))
+
+	// Get runtime base image tag (verified at startup)
+	baseImageTag := db.getRuntimeBaseImage(dh.env)
+
+	// make a folder for the function
+	// mkdir <folder>
+	dh.filePath = path.Join(TmpDir, dh.uniqueName)
+
+	err = os.MkdirAll(dh.filePath, 0777)
+	if err != nil {
+		return nil, err
+	}
+
+	// Copy the runtime Dockerfile (already configured to use the base image)
+	dockerfilePath := path.Join(runtimesDir, dh.env, "Dockerfile")
+	dockerfileData, err := runtimes.ReadFile(dockerfilePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read runtime Dockerfile: %w", err)
+	}
+
+	err = os.WriteFile(path.Join(dh.filePath, "Dockerfile"), dockerfileData, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	db.logger.Info("Function runtime", zap.String("env", dh.env), zap.String("baseImage", baseImageTag))
+
+	// copy function into folder
+	// cp <file> <folder>/fn
+	err = os.MkdirAll(path.Join(dh.filePath, "fn"), 0777)
+	if err != nil {
+		return nil, err
+	}
+
+	err = util.CopyAll(filedir, path.Join(dh.filePath, "fn"))
+	if err != nil {
+		return nil, err
+	}
+
+	// build image (now just adding function layer on top of base)
+	// docker build -t <image> <folder>
+	tar, err := archive.Tar(dh.filePath, compression.None)
+	if err != nil {
+		return nil, err
+	}
+
+	db.logger.Info("building image", zap.String("name", dh.name))
+	r, err := db.client.ImageBuild(
+		ctx,
+		tar,
+		client.ImageBuildOptions{
+			Tags:       []string{dh.uniqueName},
+			Remove:     true,
+			Dockerfile: "Dockerfile",
+			Labels: map[string]string{
+				"tinyfaas-function": dh.name,
+				"tinyFaaS":          db.tinyFaaSID,
+			},
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	defer r.Body.Close()
+	// _, err = io.Copy(io.Discard, r.Body)
+	// if err != nil {
+	// }
+	termFd, isTerminal := term.GetFdInfo(os.Stdout)
+
+	// 4. Use jsonmessage to print progress AND catch errors
+	// This function blocks until the stream is closed (build finished)
+	err = jsonmessage.DisplayJSONMessagesStream(
+		r.Body,
+		os.Stdout,
+		termFd,
+		isTerminal,
+		nil, // auxCallback (optional: used to capture the final Image ID)
+	)
+
+	// Create per-function isolated network
+	// Each function gets its own network, so functions cannot directly communicate with each other
+	// They can only reach the host via the gateway
+	db.logger.Info("creating isolated network", zap.String("name", dh.name))
+	networkResp, err := db.client.NetworkCreate(
+		ctx,
+		dh.uniqueName,
+		client.NetworkCreateOptions{
+			Driver: "bridge",
+			// Enable isolation - containers can only access the gateway, not other networks
+			Internal: false, // Must be false to allow internet/host access
+			Labels: map[string]string{
+				"tinyfaas-function": dh.name,
+				"tinyFaaS":          db.tinyFaaSID,
+			},
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	dh.network = networkResp.ID
+	dh.networkName = dh.uniqueName
+
+	e := make([]string, 0, len(envs))
+
+	for k, v := range envs {
+		e = append(e, fmt.Sprintf("%s=%s", k, v))
+	}
+
+	// Inject the full gateway URL so function handlers can call other functions
+	// regardless of the port the gateway is running on.
+	gatewayURL := fmt.Sprintf("http://%s:%s", db.publicDomain, db.gatewayPort)
+	e = append(e, fmt.Sprintf("TINYFAAS_GATEWAY_URL=%s", gatewayURL))
+
+	// Build extra hosts for --add-host
+	// This maps the public domain to the host gateway (where the Gateway is running)
+	// Using "host-gateway" is a special Docker value that resolves to the host's IP
+	extraHosts := []string{}
+	if db.publicDomain != "" && db.gatewayIP != "" {
+		extraHosts = append(extraHosts, fmt.Sprintf("%s:%s", db.publicDomain, db.gatewayIP))
+		db.logger.Info("adding extra host", zap.String("host", db.publicDomain), zap.String("ip", db.gatewayIP))
+	}
+
+	// Merge custom labels with system labels
+	containerLabels := map[string]string{
+		"tinyfaas-function": dh.name,
+		"tinyFaaS":          db.tinyFaaSID,
+	}
+	for k, v := range labels {
+		containerLabels[k] = v
+	}
+
+	// Store config for later container creation in Start()
+	dh.envVars = e
+	dh.extraHosts = extraHosts
+	dh.containerLabels = containerLabels
+
+	// NOTE: Containers are NOT created here - they will be created in Start()
+	// This ensures cold start measurement includes container creation time
+
+	// remove folder
+	// rm -rf <folder>
+	err = os.RemoveAll(dh.filePath)
+	if err != nil {
+		return nil, err
+	}
+	return dh, nil
+
+}
