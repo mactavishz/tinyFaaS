3318061dbd50e6b272decd5babd19da6907702f1
mactavishz <maczhanchao@yahoo.com>
2026-04-05 18:23:15 +0200

    Adapt integration tests

--- test/integrations/example_functions_test.go
+++ test/integrations/example_functions_test.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/OpenFogStack/tinyFaaS/pkg/manager"
 	"github.com/OpenFogStack/tinyFaaS/test/testutil"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -19,10 +20,15 @@ func TestExampleFunctions(t *testing.T) {
 	baseURL := testutil.RequireTinyFaaS(t)
 	fixturesDir := testutil.FunctionsDir(t)
 
+	resourceLimits := &manager.FunctionResources{
+		Memory: "256Mi",
+		CPU:    "500m",
+	}
+
 	t.Run("sieve", func(t *testing.T) {
 		fnName := testutil.UniqueFunctionName("sieve")
 		testutil.CleanupDeleteFunction(t, baseURL, fnName)
-		testutil.UploadFixtureFunction(t, baseURL, filepath.Join(fixturesDir, "sieve-of-eratosthenes"), fnName, "nodejs", 1, nil)
+		testutil.UploadFixtureFunction(t, baseURL, filepath.Join(fixturesDir, "sieve-of-eratosthenes"), fnName, "nodejs", 1, resourceLimits)
 
 		status, _ := testutil.Invoke(t, baseURL, fnName, http.MethodGet, nil, nil)
 		assert.Equal(t, http.StatusOK, status)
@@ -31,7 +37,7 @@ func TestExampleFunctions(t *testing.T) {
 	t.Run("sieve-async", func(t *testing.T) {
 		fnName := testutil.UniqueFunctionName("sieve-async")
 		testutil.CleanupDeleteFunction(t, baseURL, fnName)
-		testutil.UploadFixtureFunction(t, baseURL, filepath.Join(fixturesDir, "sieve-of-eratosthenes"), fnName, "nodejs", 1, nil)
+		testutil.UploadFixtureFunction(t, baseURL, filepath.Join(fixturesDir, "sieve-of-eratosthenes"), fnName, "nodejs", 1, resourceLimits)
 
 		headers := map[string]string{"X-Tinyfaas-Async": "true"}
 		status, _ := testutil.Invoke(t, baseURL, fnName, http.MethodGet, nil, headers)
@@ -41,7 +47,7 @@ func TestExampleFunctions(t *testing.T) {
 	t.Run("echo-py", func(t *testing.T) {
 		fnName := testutil.UniqueFunctionName("echo-py")
 		testutil.CleanupDeleteFunction(t, baseURL, fnName)
-		testutil.UploadFixtureFunction(t, baseURL, filepath.Join(fixturesDir, "echo-py"), fnName, "python3", 1, nil)
+		testutil.UploadFixtureFunction(t, baseURL, filepath.Join(fixturesDir, "echo-py"), fnName, "python3", 1, resourceLimits)
 
 		payload := []byte("Hello World!")
 		status, body := testutil.Invoke(t, baseURL, fnName, http.MethodPost, payload, nil)
@@ -52,7 +58,7 @@ func TestExampleFunctions(t *testing.T) {
 	t.Run("echo-js", func(t *testing.T) {
 		fnName := testutil.UniqueFunctionName("echo-js")
 		testutil.CleanupDeleteFunction(t, baseURL, fnName)
-		testutil.UploadFixtureFunction(t, baseURL, filepath.Join(fixturesDir, "echo-js"), fnName, "nodejs", 1, nil)
+		testutil.UploadFixtureFunction(t, baseURL, filepath.Join(fixturesDir, "echo-js"), fnName, "nodejs", 1, resourceLimits)
 
 		payload := []byte("Hello World!")
 		status, body := testutil.Invoke(t, baseURL, fnName, http.MethodPost, payload, nil)
@@ -63,7 +69,7 @@ func TestExampleFunctions(t *testing.T) {
 	t.Run("echo-go", func(t *testing.T) {
 		fnName := testutil.UniqueFunctionName("echo-go")
 		testutil.CleanupDeleteFunction(t, baseURL, fnName)
-		testutil.UploadFixtureFunction(t, baseURL, filepath.Join(fixturesDir, "echo-go"), fnName, "go", 1, nil)
+		testutil.UploadFixtureFunction(t, baseURL, filepath.Join(fixturesDir, "echo-go"), fnName, "go", 1, resourceLimits)
 
 		payload := []byte("Hello World!")
 		status, body := testutil.Invoke(t, baseURL, fnName, http.MethodPost, payload, nil)
@@ -74,7 +80,7 @@ func TestExampleFunctions(t *testing.T) {
 	t.Run("echo-binary", func(t *testing.T) {
 		fnName := testutil.UniqueFunctionName("echo-binary")
 		testutil.CleanupDeleteFunction(t, baseURL, fnName)
-		testutil.UploadFixtureFunction(t, baseURL, filepath.Join(fixturesDir, "echo-binary"), fnName, "binary", 1, nil)
+		testutil.UploadFixtureFunction(t, baseURL, filepath.Join(fixturesDir, "echo-binary"), fnName, "binary", 1, resourceLimits)
 
 		payload := []byte("Hello World!")
 		status, body := testutil.Invoke(t, baseURL, fnName, http.MethodPost, payload, nil)
@@ -85,7 +91,7 @@ func TestExampleFunctions(t *testing.T) {
 	t.Run("show-headers-js", func(t *testing.T) {
 		fnName := testutil.UniqueFunctionName("headers-js")
 		testutil.CleanupDeleteFunction(t, baseURL, fnName)
-		testutil.UploadFixtureFunction(t, baseURL, filepath.Join(fixturesDir, "show-headers-js"), fnName, "nodejs", 1, nil)
+		testutil.UploadFixtureFunction(t, baseURL, filepath.Join(fixturesDir, "show-headers-js"), fnName, "nodejs", 1, resourceLimits)
 
 		headers := map[string]string{"lab": "scalable_software_systems_group"}
 		status, body := testutil.Invoke(t, baseURL, fnName, http.MethodGet, nil, headers)
@@ -93,6 +99,7 @@ func TestExampleFunctions(t *testing.T) {
 
 		var got map[string]string
 		require.NoError(t, json.Unmarshal(body, &got))
+		t.Logf("Received headers in response: %v", got)
 		assert.Equal(t, "scalable_software_systems_group", got["lab"])
 		assert.Contains(t, got["user-agent"], "Go-http-client")
 	})
@@ -100,15 +107,16 @@ func TestExampleFunctions(t *testing.T) {
 	t.Run("show-headers-py", func(t *testing.T) {
 		fnName := testutil.UniqueFunctionName("headers-py")
 		testutil.CleanupDeleteFunction(t, baseURL, fnName)
-		testutil.UploadFixtureFunction(t, baseURL, filepath.Join(fixturesDir, "show-headers"), fnName, "python3", 1, nil)
+		testutil.UploadFixtureFunction(t, baseURL, filepath.Join(fixturesDir, "show-headers-py"), fnName, "python3", 1, resourceLimits)
 
-		headers := map[string]string{"Lab": "scalable_software_systems_group"}
+		headers := map[string]string{"lab": "scalable_software_systems_group"}
 		status, body := testutil.Invoke(t, baseURL, fnName, http.MethodGet, nil, headers)
 		require.Equal(t, http.StatusOK, status)
 
 		var got map[string]string
 		require.NoError(t, json.Unmarshal(body, &got))
-		assert.Equal(t, "scalable_software_systems_group", got["Lab"])
-		assert.Contains(t, got["User-Agent"], "Go-http-client")
+		t.Logf("Received headers in response: %v", got)
+		assert.Equal(t, "scalable_software_systems_group", got["lab"])
+		assert.Contains(t, got["user-agent"], "Go-http-client")
 	})
 }
