e3d3f7ff22498bbff16325f2003716a6de3b3eef
mactavishz <maczhanchao@yahoo.com>
2026-04-11 10:07:01 +0200

    Update down script for tinyfaas

--- down.sh
+++ down.sh
@@ -6,6 +6,12 @@ echo "==> Stopping existing tinyFaaS services..."
 sudo systemctl disable --now tf-manager 2>/dev/null || echo "tf-manager not running"
 sudo systemctl disable --now tf-rproxy 2>/dev/null || echo "tf-rproxy not running"
 sudo systemctl disable --now tf-gateway 2>/dev/null || echo "tf-gateway not running"
+
+echo "==> Flushing old logs..."
+sudo journalctl --rotate --vacuum-time=1s -u tf-manager || true
+sudo journalctl --rotate --vacuum-time=1s -u tf-rproxy || true
+sudo journalctl --rotate --vacuum-time=1s -u tf-gateway || true
+
 sudo rm -f /etc/default/tinyfaas
 
 echo "==> Uninstalling tinyFaaS services..."
