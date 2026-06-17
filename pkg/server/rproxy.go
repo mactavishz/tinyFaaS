b6d8247e597c6687b4db51ecbeef80b8ed536708
mactavishz <12948083+mactavishz@users.noreply.github.com>
2026-06-17 22:25:05 +0200

    Revert "Increase prewarm concurrency and per call limit"
    This reverts commit 0d4a6b79f6cec38b5eda3bb4e1b74b52f9ec0a7c.

--- pkg/server/rproxy.go
+++ pkg/server/rproxy.go
@@ -49,8 +49,8 @@ type PrewarmTuning struct {
 // DefaultPrewarmTuning returns reasonable defaults
 func DefaultPrewarmTuning() PrewarmTuning {
 	return PrewarmTuning{
-		Concurrency:  3,
-		PerCallLimit: 4,
+		Concurrency:  2,
+		PerCallLimit: 2,
 		MinSavings:   100 * time.Millisecond,
 		SafetyMargin: 50 * time.Millisecond,
 	}
